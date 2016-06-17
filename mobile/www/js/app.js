f7035c7df0077f6779162000c0da9a3d5318b126
Arthur Flôr <arthurflor23@gmail.com>
2016-06-17 01:04:53 -0300

    up

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -1,16 +1,15 @@
 function onAppReady() {
 	
-    $('#mainpage').removeClass('starting-hide').addClass('hidden');
     login_manager();
-	
-	moment.locale('pt-br');
-
-	$("#aba-dia1").html(moment().format("L"));
-	$("#aba-dia2").html(moment().add(1, 'day').format("L"));
-	$("#aba-dia3").html(moment().add(2, 'day').format("L"));
-	$("#aba-dia4").html(moment().add(3, 'day').format("L"));
-	$("#aba-dia5").html(moment().add(4, 'day').format("L"));
-	$("#aba-dia6").html(moment().add(5, 'day').format("L"));
-	$("#aba-dia7").html(moment().add(6, 'day').format("L"));
+    $("#input_date_select").datepicker({showOn:"none"}).datepicker("setDate", new Date());
+    
+//	moment.locale('pt-br');
+//	$("#input_date_select").val(moment().format("LL"));
+//	$("#aba-dia2").html(moment().add(1, 'day').format("L"));
+//	$("#aba-dia3").html(moment().add(2, 'day').format("L"));
+//	$("#aba-dia4").html(moment().add(3, 'day').format("L"));
+//	$("#aba-dia5").html(moment().add(4, 'day').format("L"));
+//	$("#aba-dia6").html(moment().add(5, 'day').format("L"));
+//	$("#aba-dia7").html(moment().add(6, 'day').format("L"));
 }
-document.addEventListener("app.Ready", onAppReady, false) ;
+document.addEventListener("app.Ready", onAppReady, false);
--- mobile/www/js/auditService/login.js
+++ mobile/www/js/auditService/login.js
@@ -3,45 +3,88 @@ $(document).on("click", "#btn_entrar_login", function(evt){
 });
 
 function login_manager(){
-	if(localStorage.getItem("key")){
-		$("#mainpage").show(); 
-		$("#page_login").hide();
+	if(!localStorage.getItem("key")){
+        activate_page("#page_login");
 	}
 }
 
 function login(){
 	
+    $('#input_login_login').blur();
+    $('#input_password_login').blur();
 	cordova.plugin.pDialog.init({cancelable:false,title:"Audit Supervisor",message:"Carregando informações..."});
+    var net = internet();
+    var validate = login_validate();
 		
-	if(internet()){
+	if(net && validate){
+        
+        var login = $("#input_login_login").val();
+        var senha = $("#input_password_login").val();
+        var senha_md5 = md5(senha);
+        var imei = cordova.plugins.uid.IMEI;
+        
+//        navigator.notification.alert(login+'\n'+senha+'\n'+senha_md5+'\n'+imei,'','','OK');
+        
+//        var data = {'login':login, 'senha':senha_md5, 'imei':imei};
+        
 //		var login = webservice(data);
 		var login = true;
 		if (login){
 
 			localStorage.setItem("login","login");
-			localStorage.setItem("id","id");
 			localStorage.setItem("key","key");
-
+			localStorage.setItem("id","id");
+            
 			cordova.plugin.pDialog.dismiss();
-			$("#mainpage").show(); 
-			$("#page_login").hide();
-
+            activate_page("#mainpage");
+            
 		} else{
 			cordova.plugin.pDialog.dismiss();		
-			navigator.notification.alert("Não foi possível entrar no sistema. Login ou senha incorreto.","","Audit Supervisor","OK");
+			navigator.notification.alert("Não foi possível entrar no sistema.\nLogin ou senha incorreto.","","Audit Supervisor","OK");
 		}
 	} else{
-		cordova.plugin.pDialog.dismiss();		
-		navigator.notification.alert("Não foi possível entrar no sistema. Verifique sua conexão com a internet.","","Audit Supervisor","OK");
+		cordova.plugin.pDialog.dismiss();	
+        
+        if(!net){
+            navigator.notification.alert("Não foi possível entrar no sistema.\nVerifique sua conexão com a internet.","","Audit Supervisor","OK");
+        } else if(!validate){
+            navigator.notification.alert("Não foi possível entrar no sistema.\nVerifique seu login e/ou senha.","","Audit Supervisor","OK");
+        }
 	}
 }
 
 function logout(){
-	cordova.plugin.pDialog.init({cancelable:false,title:"Audit Supervisor",message:"Carregando informações..."});
+	cordova.plugin.pDialog.init({cancelable:false,title:"Audit Supervisor",message:"Saindo do sistema..."});
 		localStorage.setItem("login","");
 		localStorage.setItem("id","");
 		localStorage.setItem("key","");
-		$(".upage").hide();
-		$("#page_login").show();
+    
+        activate_page("#page_login");
+    
 	cordova.plugin.pDialog.dismiss();	
+}
+
+function login_validate(){
+    
+    var check=false;
+    
+    if($("#input_login_login").val()==""){
+        $("#input_login_branco").removeClass("hidden");
+    } else{
+        $("#input_login_branco").addClass("hidden");
+    }
+    
+    if($("#input_password_login").val()==""){
+        $("#input_password_branco").removeClass("hidden");
+    } else{
+        $("#input_password_branco").addClass("hidden");
+    }
+    
+    if($("#input_login_login").val()=="" || $("#input_password_login").val()==""){
+        check=false;
+    } else {
+        check=true;
+    }
+    
+    return check;
 }
