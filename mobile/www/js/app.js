ccad7d664bdaadd4c00fef5813d8dc419a254c3d
Arthur Flôr <arthurflor23@gmail.com>
2016-06-18 17:33:47 -0300

    up

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -1,4 +1,5 @@
 function onAppReady() {
+    
     $("#input_date_select").change(function() {
 //        navigator.notification.alert("Handler for .change() called.");
     });
@@ -6,6 +7,8 @@ function onAppReady() {
         $("#input_date_select").val($("#icon_date_select").val());
 //        navigator.notification.alert("Handler for .change() called.");
     });
+
+    expandTextarea();
     
     login_manager();
     $("#input_date_select").datepicker().datepicker("setDate", new Date());
@@ -17,3 +20,20 @@ function onAppReady() {
 //	$("#aba-dia3").html(moment().add(2, 'day').format("L"));
 }
 document.addEventListener("app.Ready", onAppReady, false);
+
+function expandTextarea() {
+    
+    var $element = $('.autosize');  
+    
+    $('.expand').each(function() {
+        $(this).attr('rows', Math.ceil($(this).val().length / ($(this).width()/5)) * 0.69);
+    });
+    
+    for (i=0; i<$element.length; i++){
+        $element[i].addEventListener('keyup', function() {
+            this.style.overflow = 'hidden';
+            this.style.height = 0;
+            this.style.height = this.scrollHeight + 'px';
+        }, false);
+    }
+}
--- mobile/www/js/index_user_scripts.js
+++ mobile/www/js/index_user_scripts.js
@@ -1,14 +1,15 @@
-///*jshint browser:true */
-///*global $ */(function()
-//{
-// "use strict";
-// /*
-//   hook up event handlers 
-// */
-// function register_event_handlers()
-// {
-//
-//    
-//    }
-// document.addEventListener("app.Ready", register_event_handlers, false);
-//})();
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+
+     
+    
+  }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
