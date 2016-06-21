07317144c326dc86bc7b04e1b79ff976a6ba55d6
Arthur Flôr <arthurflor23@gmail.com>
2016-06-21 10:39:22 -0300

    up

--- mobile/www/js/index_user_scripts.js
+++ mobile/www/js/index_user_scripts.js
@@ -1,12 +1,6 @@
-/*jshint browser:true */
-/*global $ */(function()
-{
+(function(){
  "use strict";
- /*
-   hook up event handlers 
- */
- function register_event_handlers()
- {
+ function register_event_handlers(){
      
      $(document).on("click", "#limparform", function(evt){
          limpar_campos();
@@ -15,7 +9,64 @@
      $(document).on("click", "#limparfeedback", function(evt){
          limpar_campos();
      });
-    
   }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+function expandTextarea() {
+    
+    var $element = $('.autosize');  
+    
+    $('.expand').each(function() {
+        $(this).attr('rows', Math.ceil($(this).val().length / ($(this).width()/5)) * 0.69);
+    });
+    
+    for (i=0; i<$element.length; i++){
+        $element[i].addEventListener('keyup', function() {
+            this.style.overflow = 'hidden';
+            this.style.height = 0;
+            this.style.height = this.scrollHeight + 'px';
+        }, false);
+    }
+}
+
+function limpar_campos(){
+    
+    cordova.plugins.Keyboard.close();
+    
+    $('textarea').each(function() {
+        $(this).blur();
+    });
+    
+    $('input').each(function() {
+        $(this).blur();
+    });
+    
+    $('textarea').each(function() {
+        if($(this).hasClass('autosize')){
+            $(this).val("");
+        }
+    });
+    
+    $('[type=checkbox]').each(function() {
+        $(this).prop("checked", false);
+    });
+}
+
+
+function formatDate(stringDate){
+    
+    var day = stringDate.substring(8,10);
+    var month = stringDate.substring(5,7);
+    var year = stringDate.substring(0,4);
+    
+    return day + "/" + month + "/" + year;
+}
+
+function MainCtrl($scope, $ionicScrollDelegate) {
+    $scope.scrollMainToTop = function() {
+        $ionicScrollDelegate.$getByHandle('mainScroll').scrollTop();
+    };
+}
