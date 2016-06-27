714bbee59fb91dbe0b19bc26c5f9a34d63403c2c
berg <vandemberg.silva.lima@gmail.com>
2016-06-27 17:45:42 -0300

    webservice - visitas

--- web/cardial/app/Http/Controllers/WebServiceController.php
+++ web/cardial/app/Http/Controllers/WebServiceController.php
@@ -48,24 +48,42 @@ class WebServiceController extends Controller {
             if (empty($usuario))
                 return json_encode(array("false"));
 
-            if ($supervisor->chave == $_GET['chave']) {
+            if ($usuario->chave == $_GET['chave']) {
                 $dados = array("true", array(), array());
 
                 // estrutura dos dados
+                // busca as visitas de hoje até 29 dias desse supervisor
+                $visitas = VisitaBase::
+                        join('visita', 'visita_base.id', '=', 'visita.visita_id')->
+                        join('cliente', 'visita_base.cliente_id', '=', 'cliente.id')->
+                        join('vendedor', 'visita_base.vendedor_id', '=', 'vendedor.id')->
+                        join('formulario', 'visita_base.formulario_id', '=', 'formulario.id')->
+                        select('cliente.cidade', 'visita.id', 'visita.data_inicial', 'cliente.razao_social', 'vendedor.id', 'vendedor.nome', 'formulario.id')
+                        ->where('visita_base.supervisor_id', $supervisor_id)
+                        ->where('visita.data_inicial', '>', date('Y-m-d', strtotime("-1 days")))
+                        ->where('visita.data_inicial', '<=', date('Y-m-d', strtotime("+29 days")))
+                        ->get();
 
-                $dados[1][$item->data_inicio] = array(
-                    $item->cidade,
-                    $item->id,
-                    $item->razao_social,
-                    $item->vendedor_id,
-                    $item->vendedor_nome,
-                    $item->formulario_id);
+                return $visitas;
+                // monta as visitas
+//                foreach ($visitas as $key => $item) {
+//                    $dados[1][$item["data_inicial"] = array(
+//                    $item["cidade"],
+//                    $item["id"],
+//                    $item["razao_social"],
+//                    $item["vendedor_id"],
+//                    $item["vendedor_nome"],
+//                    $item["formulario_id"],
+//                    );
+                }
 
+                // monta as perguntas
                 $dados[2][$formulario_id] = array(
                     $item->id,
                     $item->descricao,
                     $item->obrigatorio,
-                    $item->ordem
+                    $item->ordem,
+                    $item->visita_id
                 );
 
                 return json_encode($dados);
